eaed28a3e51781574d6a9c18d5e51d5f5515da83
Borimir1 <bobi_aleksiev46@abv.bg>
2024-01-05 22:09:11 +0200

    Validate user input for size of the board and the mines

--- Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
-
 using namespace std;
+
+constexpr int MAX_SIZE = 10;
+
+void initializeValidGame(int& N,int& mines) {
+	cout << "Input the size of the board!" << endl;
+	cin >> N;
+	while (N < 3 || N > 10) {
+		cout << "Invalid input. Try again!" << endl;
+		cin >> N;
+	}
+	cout << "Input how many mines do you want:" << endl;
+	cin >> mines;
+	while (mines < 1 || mines > 3 * N) {
+		cout << "Invalid input. Try again!" << endl;
+		cin >> mines;
+	}
+}
 
 int main()
 {
-
+	char board[MAX_SIZE][MAX_SIZE];
+	int N = 0, mines = 0;
+	initializeValidGame(N, mines);
 }
